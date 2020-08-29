a8d05fb2c5a3d57e77a2249d59111420dd3e4454
Val <btwonu@gmail.com>
2020-08-29 20:17:24 +0300

    Fixed videos displaying over sidebar menu

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,31 +1,43 @@
-//Declarations
-const overlay = document.getElementById('overlay');
-const hamburgerBtn = document.getElementById('hamburger-menu');
-const sidebar = document.getElementById('sidebar');
-const hamburgerItems = document.querySelectorAll('.hamburger-item');
+function init() {
+  //Declarations
+  const overlay = document.getElementById('overlay');
+  const hamburgerBtn = document.getElementById('hamburger-menu');
+  const sidebar = document.getElementById('sidebar');
+  const hamburgerItems = document.querySelectorAll('.hamburger-item');
 
-//On hamburger click
-hamburgerBtn.addEventListener('click', toggleMenu);
-//On window resize
-window.addEventListener('resize', closeSidebar);
+  //On hamburger click
+  hamburgerBtn.addEventListener('click', toggleMenu);
+  //On window resize
+  window.addEventListener('resize', closeSidebar);
 
-//Declarations
-function toggleMenu() {
-  //Toggle hamburger items
-  Array.from(hamburgerItems).forEach((menuItem) => {
-    menuItem.classList.toggle('open');
-  });
+  //Declarations
+  function toggleMenu() {
+    const iframes = document.querySelectorAll('.song');
 
-  //Toggle sidebar
-  sidebar.classList.toggle('open');
-  //Toggle overlay
-  overlay.classList.toggle('visibility-visible');
-}
+    //Toggle hamburger items
+    Array.from(hamburgerItems).forEach((menuItem) => {
+      menuItem.classList.toggle('open');
+    });
 
-//If the screen is larger than what is needed to display the hamburger menu and
-//the menu is opened - close it.
-function closeSidebar() {
-  if (window.innerWidth > 640 && sidebar.classList.contains('open')) {
-    toggleMenu();
+    //Toggle sidebar
+    sidebar.classList.toggle('open');
+    //Toggle overlay
+    overlay.classList.toggle('visibility-visible');
+
+    // Move iframes below
+    Array.from(iframes).forEach((song) => {
+      song.classList.toggle('below');
+    });
+  }
+
+  //If the screen is larger than what is needed to display the hamburger menu and
+  //the menu is opened - close it.
+  function closeSidebar() {
+    if (window.innerWidth > 640 && sidebar.classList.contains('open')) {
+      toggleMenu();
+    }
   }
 }
+
+//Initialize
+window.addEventListener('DOMContentLoaded', init());
